496329572535352eea27447482f5e8798da61d1b
bilakicil <sulianini783@gmail.com>
2024-05-31 00:12:25 +0800

    Update HomePageController.java

--- Kakir-20240502T192409Z-001/Kakir/src/main/java/com/example/controller/HomePageController.java
+++ Kakir-20240502T192409Z-001/Kakir/src/main/java/com/example/controller/HomePageController.java
@@ -1,5 +1,36 @@
 package com.example.controller;
 
-public class HomePageController {
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
 
-}
+import java.io.IOException;
+import java.util.Objects;
+
+public class HomePageController {
+    @FXML
+    private void toFindPage(MouseEvent event) throws IOException {
+        Parent root = FXMLLoader.load(Objects.requireNonNull(Objects.requireNonNull(getClass().getResource("/com/example/pasien page.fxml"))));
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setResizable(true);
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+
+    }
+    @FXML
+    private void toStockPage(MouseEvent event) throws IOException {
+        Parent root = FXMLLoader.load(Objects.requireNonNull(Objects.requireNonNull(getClass().getResource("/com/example/stock page.fxml"))));
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setResizable(true);
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+
+    }
