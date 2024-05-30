2e04f23c8cac42cf80da7ef3a94ab534908f25ef
reydstry <168646639+reydstry@users.noreply.github.com>
2024-05-31 00:34:00 +0800

    Update ControllerDataBase.java

--- Kakir-20240502T192409Z-001/Kakir/src/main/java/com/example/Database/ControllerDataBase.java
+++ Kakir-20240502T192409Z-001/Kakir/src/main/java/com/example/Database/ControllerDataBase.java
@@ -1,5 +1,17 @@
 package com.example.Database;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class ControllerDataBase {
+    public static Connection getConnection(){
+        try {
+            return DriverManager.getConnection("jdbc:mysql://localhost:3306/tbobat", "root", "");
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
 }
