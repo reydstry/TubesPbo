e8cfdf30618efd13c1aa3391f63f7bd65be60c1d
Human3asu <162811872+Human3asu@users.noreply.github.com>
2024-05-31 00:54:18 +0800

    Update Dao.java

--- Kakir-20240502T192409Z-001/Kakir/src/main/java/com/example/Dao/Dao.java
+++ Kakir-20240502T192409Z-001/Kakir/src/main/java/com/example/Dao/Dao.java
@@ -1,5 +1,15 @@
 package com.example.Dao;
+import java.sql.Connection;
+import java.util.ArrayList;
 
 public abstract class Dao<T> {
+    protected Connection connection;
+
+    public Dao(Connection connection){
+        this.connection = connection;
+    }
+    public abstract T getById(int id);
+
+    public abstract ArrayList<T> getAll();
 
 }
