3c8778fe56ff597834aa3c2c68a367368658ce42
Human3asu <162811872+Human3asu@users.noreply.github.com>
2024-05-31 00:52:08 +0800

    Update Obat.java

--- Kakir-20240502T192409Z-001/Kakir/src/main/java/com/example/kasir/Obat.java
+++ Kakir-20240502T192409Z-001/Kakir/src/main/java/com/example/kasir/Obat.java
@@ -1,5 +1,72 @@
 package com.example.kasir;
 
 public class Obat {
+    private String nameObat;
+    private double harga;
+    private String jenisObat;
+    private int dosis;
+    private String keluhan;
+    private int jumlah; 
 
+    public int getJumlah() {
+        return jumlah;
+    }
+
+    public void setJumlah(int jumlah) {
+        this.jumlah = jumlah;
+    }
+
+    public Obat(String nameObat, double harga, String jenisObat, int dosis, String keluhan) {
+        this.nameObat = nameObat;
+        this.harga = harga;
+        this.jenisObat = jenisObat;
+        this.dosis = dosis;
+        this.keluhan = keluhan;
+    }
+
+    public String getNameObat() {
+        return nameObat;
+    }
+
+    public void setNameObat(String nameObat) {
+        this.nameObat = nameObat;
+    }
+
+    public double getHarga() {
+        return harga;
+    }
+
+    public void setHarga(double harga) {
+        this.harga = harga;
+    }
+
+    public String getJenisObat() {
+        return jenisObat;
+    }
+
+    public void setJenisObat(String jenisObat) {
+        this.jenisObat = jenisObat;
+    }
+
+    public int getDosis() {
+        return dosis;
+    }
+
+    public void setDosis(int dosis) {
+        this.dosis = dosis;
+    }
+
+    public String getKeluhan() {return keluhan;}
+
+    public void setKeluhan(String keluhan) {this.keluhan = keluhan;}
+    @Override
+    public String toString() {
+        return "Obat{" +
+                "nameObat='" + nameObat + '\'' +
+                ", harga=" + harga +
+                ", jenisObat='" + jenisObat + '\'' +
+                ", dosis=" + dosis +
+                ", keluhan='" + keluhan + '\'' +
+                '}';
+    }
 }
