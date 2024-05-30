ae84b629d4a472b1379fc5667b2f664b3892a9da
bilakicil <sulianini783@gmail.com>
2024-05-31 00:15:59 +0800

    Update StockPageController.java

--- Kakir-20240502T192409Z-001/Kakir/src/main/java/com/example/controller/StockPageController.java
+++ Kakir-20240502T192409Z-001/Kakir/src/main/java/com/example/controller/StockPageController.java
@@ -1,5 +1,33 @@
 package com.example.controller;
 
-public class StockPageController {
+import com.example.Database.ControllerDataBase;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.TextField;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
 
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.Objects;
+
+public class StockPageController {
+ @FXML
+    private void toFindPage(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(Objects.requireNonNull(Objects.requireNonNull(getClass().getResource("/com/example/pasien page.fxml"))));
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setResizable(true);
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+
+    }
 }
