d241483b016f9f731290f1936604cb66235ce2f1
reydstry <168646639+reydstry@users.noreply.github.com>
2024-06-02 09:13:54 +0800

    Update HomePageController.java

--- Kakir-20240502T192409Z-001/Kakir/src/main/java/com/example/controller/HomePageController.java
+++ Kakir-20240502T192409Z-001/Kakir/src/main/java/com/example/controller/HomePageController.java
@@ -9,13 +9,48 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
-
 import java.io.IOException;
 import java.util.Objects;
 
 public class HomePageController {
+
+    @FXML
+    private ImageView cariObat;
+    @FXML
+    private ImageView tambahObat;
+
+    @FXML
+    private void initialize() {
+
+        cariObat.setOnMouseEntered(event -> {
+            cariObat.setImage(new Image("com/example/image/cari3.png"));
+        });
+
+        cariObat.setOnMouseExited(event -> {
+            cariObat.setImage(new Image("com/example/image/cari.png"));
+        });
+
+        cariObat.setOnMousePressed(event -> {
+            cariObat.setImage(new Image("com/example/image/cari2.png"));
+        });
+
+        tambahObat.setOnMouseEntered(event -> {
+            tambahObat.setImage(new Image("com/example/image/tambah3.png"));
+        });
+
+        tambahObat.setOnMouseExited(event -> {
+            tambahObat.setImage(new Image("com/example/image/tambah.png"));
+        });
+
+        tambahObat.setOnMousePressed(event -> {
+            tambahObat.setImage(new Image("com/example/image/tambah2.png"));
+        });
+
+    }
+
     @FXML
     private void toFindPage(MouseEvent event) throws IOException {
+
         Parent root = FXMLLoader.load(Objects.requireNonNull(Objects.requireNonNull(getClass().getResource("/com/example/pasien page.fxml"))));
         Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         stage.setResizable(true);
@@ -26,39 +61,13 @@ public class HomePageController {
     }
     @FXML
     private void toStockPage(MouseEvent event) throws IOException {
+
         Parent root = FXMLLoader.load(Objects.requireNonNull(Objects.requireNonNull(getClass().getResource("/com/example/stock page.fxml"))));
         Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         stage.setResizable(true);
         Scene scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
-
-    }
-    @FXML
-    private ImageView cariObat;
-    @FXML
-    private ImageView tambahObat;
-
-    @FXML
-    private void initialize() {
-        cariObat.setOnMouseEntered(event -> {
-            cariObat.setImage(new Image("com/example/image/cari3.png"));
-        });
-        cariObat.setOnMouseExited(event -> {
-            cariObat.setImage(new Image("com/example/image/cari.png"));
-        });
-        cariObat.setOnMousePressed(event -> {
-            cariObat.setImage(new Image("com/example/image/cari2.png"));
-        });
-        tambahObat.setOnMouseEntered(event -> {
-            tambahObat.setImage(new Image("com/example/image/tambah3.png"));
-        });
-        tambahObat.setOnMouseExited(event -> {
-            tambahObat.setImage(new Image("com/example/image/tambah.png"));
-        });
-        tambahObat.setOnMousePressed(event -> {
-            tambahObat.setImage(new Image("com/example/image/tambah2.png"));
-        });
 
     }
 }
