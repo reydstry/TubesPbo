4518b16dd1dec159127058ac08e7ce847831c6aa
reydstry <168646639+reydstry@users.noreply.github.com>
2024-06-02 09:18:08 +0800

    Update Obat.java

--- Kakir-20240502T192409Z-001/Kakir/src/main/java/com/example/kasir/Obat.java
+++ Kakir-20240502T192409Z-001/Kakir/src/main/java/com/example/kasir/Obat.java
@@ -4,69 +4,61 @@ public class Obat {
     private String nameObat;
     private double harga;
     private String jenisObat;
-    private int dosis;
+    private int stock;
     private String keluhan;
-    private int jumlah; 
+    private int jumlah;
 
-    public int getJumlah() {
-        return jumlah;
-    }
-
-    public void setJumlah(int jumlah) {
-        this.jumlah = jumlah;
-    }
-
-    public Obat(String nameObat, double harga, String jenisObat, int dosis, String keluhan) {
+    public Obat(String nameObat, double harga, String jenisObat, int stock, String keluhan) {
         this.nameObat = nameObat;
         this.harga = harga;
         this.jenisObat = jenisObat;
-        this.dosis = dosis;
+        this.stock = stock;
         this.keluhan = keluhan;
+    }
+
+    public Obat(String nameObat, double harga, String jenisObat, int stock, String keluhan, int jumlah) {
+        this(nameObat, harga, jenisObat, stock, keluhan);
+        this.jumlah = jumlah;
     }
 
     public String getNameObat() {
         return nameObat;
     }
 
-    public void setNameObat(String nameObat) {
-        this.nameObat = nameObat;
-    }
-
     public double getHarga() {
         return harga;
-    }
-
-    public void setHarga(double harga) {
-        this.harga = harga;
     }
 
     public String getJenisObat() {
         return jenisObat;
     }
 
-    public void setJenisObat(String jenisObat) {
-        this.jenisObat = jenisObat;
+    public int getStock() {
+        return stock;
     }
 
-    public int getDosis() {
-        return dosis;
+    public String getKeluhan() {
+        return keluhan;
     }
 
-    public void setDosis(int dosis) {
-        this.dosis = dosis;
+    public int getJumlah() {
+        return jumlah;
     }
 
-    public String getKeluhan() {return keluhan;}
+    public int setJumlah(int jumlah) {
+        this.jumlah = jumlah;
+        return jumlah;
+    }
 
-    public void setKeluhan(String keluhan) {this.keluhan = keluhan;}
     @Override
     public String toString() {
         return "Obat{" +
                 "nameObat='" + nameObat + '\'' +
                 ", harga=" + harga +
                 ", jenisObat='" + jenisObat + '\'' +
-                ", dosis=" + dosis +
+                ", dosis=" + stock +
                 ", keluhan='" + keluhan + '\'' +
+                ", jumlah=" + jumlah +
                 '}';
     }
 }
